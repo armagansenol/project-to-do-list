9a4805991af68e362aaee2822719abbadc06fe14
armagansenol <armagansnl@gmail.com>
2020-06-10 22:50:50 +0300

    gitignore dist erased

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/project-to-do-list/",
     },
     module: {
         rules: [{
